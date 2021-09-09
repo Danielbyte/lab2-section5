fb774229240e88a892b70774cafd70661689f780
Danielbyte <danielmasango7@gmail.com>
2021-09-09 19:30:33 +0200

    Exercise 5.2

--- src/text-query-system/Line.cpp
+++ src/text-query-system/Line.cpp
@@ -3,14 +3,36 @@
 // -------------------------------------------
 
 #include "Line.h"
+#include <algorithm>
+#include <string>
+#include <sstream>
+using namespace std;
 
 Line::Line(const string& line)
 {
 	// Hint: some of string's member functions might come in handy here
 	// for extracting words.
+	auto temp = ""s;
+	stringstream str(line);
+	while (std::getline(str, temp, ' '))
+    {
+       line_.push_back(temp);
+    }
+
 }
 
 bool Line::contains(const Word& search_word) const
 {
-	return false;
+   if (line_.empty())
+   {
+       return false;
+   }
+
+   for (auto element : line_)
+   {
+       auto temp = Word{element};
+       if(temp == search_word && search_word.isQueryable())
+       {return true;}
+
+   }
 }
